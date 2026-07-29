d6fc3b60a574088585f70758cd187f092b7f0601
Shikhar <shikhar0538@gmail.com>
2026-07-29 15:04:02 +0530

    uncommented the learning part

--- src/UserInput.java
+++ src/UserInput.java
@@ -3,35 +3,35 @@ import java.util.Scanner;
 
 public class UserInput {
     public static void main(String[] args){
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.print("Enter your name: ");
-//
-//        String name = scanner.nextLine();
-//        System.out.print("Enter your age: ");
-//        int age = scanner.nextInt();
-//        System.out.print("What is your CGPA ?");
-//        double cgpa = scanner.nextDouble();
-////        If we want with spaces write nextLine otherwise next
-//        System.out.print("Are you a student ?(true/ false) ");
-//        boolean isStudent = scanner.nextBoolean();
-//
-//        System.out.println("hello "+ name+ " my age is "+ age+" Your CGPA is : "+cgpa);
-//
-//        if(isStudent){
-//            System.out.println("Your are enrolled as a Student");
-//        }
-//        else{
-//            System.out.println("You are not enrolled as a Student");
-//        }
-//
-//
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+
+        String name = scanner.nextLine();
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+        System.out.print("What is your CGPA ?");
+        double cgpa = scanner.nextDouble();
+//        If we want with spaces write nextLine otherwise next
+        System.out.print("Are you a student ?(true/ false) ");
+        boolean isStudent = scanner.nextBoolean();
+
+        System.out.println("hello "+ name+ " my age is "+ age+" Your CGPA is : "+cgpa);
+
+        if(isStudent){
+            System.out.println("Your are enrolled as a Student");
+        }
+        else{
+            System.out.println("You are not enrolled as a Student");
+        }
+
+//  Exercise to calculate area of rectangle
 //
 //
 //        scanner.close();
         double width = 0;
         double height = 0;
         double area = 0;
-        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the width ");
         width = scanner.nextDouble();
         System.out.print("Enter the height ");
