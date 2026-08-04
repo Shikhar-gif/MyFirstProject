2a0a2a8405927c5bd61eeeddb033c560910317bb
Shikhar <shikhar0538@gmail.com>
2026-08-04 07:02:09 +0530

    Nested If

---
+++ src/basics/NestedIf.java
@@ -0,0 +1,65 @@
+package basics;
+
+import java.util.Locale;
+
+public class NestedIf {
+    public static void main(String[] args) {
+//        boolean isStudent = false;
+//        boolean isSenior = true;
+//
+//        double price = 9.99;
+//
+//        if (isStudent) {
+//            if(isSenior){
+//                System.out.println("You get a senior discount of %20");
+//                System.out.println("You get a student discount of 10%");
+//                price *= 70;
+//
+//            }
+//            System.out.println("You get a student of 10% ");
+//            price *= 0.9;
+//        } else {
+//            if(isSenior){
+//                System.out.println("You get  senior discount of 20%");
+//                price *= 80;
+//            }else{
+//                price *= 1;
+//            }
+//
+//        }
+//        System.out.printf("The price of a ticket is: $%.2f", price);
+
+        String name = "bro Code";
+//        int length = name.length();
+//       finding index of the char
+//        int idx = name.indexOf("o");
+//        int lastIndex = name.lastIndexOf("o");
+//        char letter = name.charAt(5);
+
+//        System.out.println(letter);
+//        System.out.println(idx);
+//        System.out.println(lastIndex);
+//
+//
+//        name = name.toUpperCase();
+//        name = name.toLowerCase();
+//        name = name.trim();
+//        name = name.replace("o","a");
+//        System.out.println(name);
+
+        if(name.isEmpty()){
+            System.out.println("Your name is Empty");
+
+        }
+        else{
+            System.out.println("Hello "+ name);
+        }
+
+
+
+
+
+
+
+    }
+}
